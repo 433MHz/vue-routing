793a3fbeb6bea8d78edd00fd28a9f14bad70006f
DESKTOP-8TBEVD4\Krystian Izdebski <krystian.izdebski1999@gmail.com>
2021-04-26 07:20:16 +0200

    Example of routing components using router function in vue app

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>App component</h1>
+  <router-link to="/about">Link to about</router-link><br>
+  <router-link to="/">Link to home component</router-link><br>
+
+  <router-view></router-view>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>I'm about component</h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
