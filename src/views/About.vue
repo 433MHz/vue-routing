f59d74e9fe099bbb348baaf96e01a47035d5d37d
DESKTOP-8TBEVD4\Krystian Izdebski <krystian.izdebski1999@gmail.com>
2021-04-26 11:41:08 +0200

    setup data transfer example

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,15 +1,18 @@
 <template>
     <h1>About me</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam quas voluptatum, quisquam ipsum sint optio voluptate delectus, a aliquam odit fuga atque recusandae quos in temporibus. Consectetur laboriosam ab odit ex cum ipsum voluptatum unde minima officia incidunt officiis magnam tenetur provident dolorum, laborum totam perferendis iusto ea! Similique delectus obcaecati blanditiis, doloremque dolore necessitatibus magnam quas aperiam dignissimos quasi ad quibusdam, molestias totam odio facilis est. Expedita ex nam nulla perferendis est itaque nostrum officiis dolore consequatur non ipsa odit officia nemo earum explicabo ea suscipit in, nesciunt ducimus reprehenderit, dolorem eaque ipsam quos. Enim error temporibus doloremque sit.</p>
+    {{name1}}
+    {{age1}}
 
-     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam quas voluptatum, quisquam ipsum sint optio voluptate delectus, a aliquam odit fuga atque recusandae quos in temporibus. Consectetur laboriosam ab odit ex cum ipsum voluptatum unde minima officia incidunt officiis magnam tenetur provident dolorum, laborum totam perferendis iusto ea! Similique delectus obcaecati blanditiis, doloremque dolore necessitatibus magnam quas aperiam dignissimos quasi ad quibusdam, molestias totam odio facilis est. Expedita ex nam nulla perferendis est itaque nostrum officiis dolore consequatur non ipsa odit officia nemo earum explicabo ea suscipit in, nesciunt ducimus reprehenderit, dolorem eaque ipsam quos. Enim error temporibus doloremque sit.</p>
-
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam quas voluptatum, quisquam ipsum sint optio voluptate delectus, a aliquam odit fuga atque recusandae quos in temporibus. Consectetur laboriosam ab odit ex cum ipsum voluptatum unde minima officia incidunt officiis magnam tenetur provident dolorum, laborum totam perferendis iusto ea! Similique delectus obcaecati blanditiis, doloremque dolore necessitatibus magnam quas aperiam dignissimos quasi ad quibusdam, molestias totam odio facilis est. Expedita ex nam nulla perferendis est itaque nostrum officiis dolore consequatur non ipsa odit officia nemo earum explicabo ea suscipit in, nesciunt ducimus reprehenderit, dolorem eaque ipsam quos. Enim error temporibus doloremque sit.</p>
 </template>
 
 <script>
 export default {
+    setup(){
+       var name = "Krystian"
+       var age = 21
 
+       return{ name1: name, age1: age}
+    }
 }
 </script>
 
