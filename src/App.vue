f26e632413d8d0f8923714336126008b0191555f
DESKTOP-8TBEVD4\Krystian Izdebski <krystian.izdebski1999@gmail.com>
2021-04-26 07:56:43 +0200

    Example of sending params using link to component

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <h1>App component</h1>
   <router-link :to="{name:'About'}">Link to about</router-link><br>
   <router-link :to="{name:'Home'}">Link to home component</router-link><br>
+  <router-link to="/jobs/7">Link to job details</router-link><br>
 
   <router-view></router-view>
 </template>
---
+++ src/views/JobDetails.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>I'm job details</h1>
+  <h2>And its router id: {{ $route.params.id }}</h2>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
